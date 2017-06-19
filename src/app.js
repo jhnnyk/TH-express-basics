f1a40a984fe335538aa3aed180d82a9d95c4a973
John Knotts <jhnnyk@gmail.com>
2017-06-19 09:29:43 -0600

    basic express server up and running

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 'use strict'
 
-const express = require('express');
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send("I love mac 'n cheese!")
+})
+
+app.listen(3000)
