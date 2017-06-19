bf97763c5079f76c872a7096ce25968c2eeefbfa
John Knotts <jhnnyk@gmail.com>
2017-06-19 09:35:44 -0600

    complete getting started section

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send("I love mac 'n cheese!")
+app.get('/', (req, res) => {
+  res.send("<h1>I love mac 'n cheese!</h1>")
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("The frontend server is running on port 3000!");
+})
