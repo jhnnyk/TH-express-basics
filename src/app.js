d1de193fa6eed71880a162758b3ac072fa8a8f4c
John Knotts <jhnnyk@gmail.com>
2017-06-21 13:04:11 -0600

    using Express middleware to load static files

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const posts = require('./mock/posts.json')
 
 const app = express()
 
+app.use('/static', express.static(__dirname + '/public'))
+
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/templates')
 
